81e3cd301d3e92012d8a85c067894691a423cb37
Zohaib Khan <zohaib.khan@sky.uk>
2024-10-23 00:44:58 +0100

    Quartz sync: Oct 23, 2024, 12:44 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,6 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/zkhan96/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
